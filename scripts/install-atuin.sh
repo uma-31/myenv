be16a8d6c234cae30e85983c4fc7d99a3ce6c327
uMa <ysmst35@gmail.com>
2024-03-31 14:51:38 +0900

    Atuin installation does not change zshrc

--- scripts/install-atuin.sh
+++ scripts/install-atuin.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+
+SCRIPT_URL="https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh"
+
 if command -v atuin &> /dev/null; then
     echo "Skip: atuin is already installed"
     exit 0
 fi
 
-bash <(curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh)
+bash <(curl $SCRIPT_URL | sed 's/^.*eval "$(atuin init zsh)".*$/printf "Skipped zshrc change"/g')
