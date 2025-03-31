289ce759870c84940d2726147a4d584e7aaf8f97
uMa <ysmst35@gmail.com>
2025-03-31 22:26:38 +0900

    Update install script for atuin

--- scripts/install-atuin.sh
+++ scripts/install-atuin.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-
-SCRIPT_URL="https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh"
-
-if command -v atuin &> /dev/null; then
+if command -v atuin &>/dev/null; then
     echo "Skip: atuin is already installed"
     exit 0
 fi
 
-bash <(curl $SCRIPT_URL | sed 's/^.*eval "$(atuin init zsh)".*$/printf "Skipped zshrc change"/g')
+bash <(
+    curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh |
+        sed 's/^.*eval "$(atuin init zsh)".*$/printf "Skipped zshrc change"/g'
+)
