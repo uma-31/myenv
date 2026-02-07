e33eaff68dd1d0853f501dfa709e2bf9b8a9661c
uMa <ysmst35@gmail.com>
2026-02-07 23:50:13 +0900

    init で ssh 設定と必須のキーを生成するようにした

--- .chezmoitemplates/init.sh
+++ .chezmoitemplates/init.sh
@@ -35,12 +35,22 @@ __setup_zsh_as_login_shell() {
     chsh -s "$zsh_path"
 }
 
+__setup_ssh_config() {
+    local ssh_conf_dir="$HOME/.ssh/conf.d"
+
+    myenv_log_info "SSH 設定用のディレクトリを作成します: $ssh_conf_dir"
+    mkdir -p ~/.ssh/conf.d
+
+    myenv_log_info "GitHub 用の SSH 設定を生成します。"
+    ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519_github
+}
+
 myenv_init() {
     myenv_log_info "初期セットアップを開始します"
 
     __remove_bootstrap_chezmoi
     __install_mise_tools
     __setup_zsh_as_login_shell
-
+    __setup_ssh_config
     myenv_log_info "セットアップが完了しました！${LF}シェルを再起動してください。"
 }
