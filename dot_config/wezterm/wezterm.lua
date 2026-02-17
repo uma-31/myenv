2f25b5044acf220e23b66f81ff85c5a106172a18
uMa <ysmst35@gmail.com>
2026-02-17 11:07:09 +0900

    Wezterm で環境固有の設定を読み込めるようにした

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,8 @@
 local wezterm = require "wezterm"
-
 local config = wezterm.config_builder()
 
+-- 外観設定
 config.color_scheme = "GitHub Dark"
-
 config.font = wezterm.font("HackGen Console NF")
 
 config.background = {
@@ -20,7 +19,6 @@ config.background = {
 local background_image_candidates = wezterm.glob(
     wezterm.config_dir .. "/background-image.*"
 )
-
 if #background_image_candidates > 0 then
     table.insert(
         config.background,
@@ -35,9 +33,20 @@ if #background_image_candidates > 0 then
     )
 end
 
+-- カーソル設定
 config.cursor_blink_rate = 800
 config.default_cursor_style = "BlinkingBar"
 
+-- タブバー設定
 config.hide_tab_bar_if_only_one_tab = true
+
+-- Mac OS
+config.native_macos_fullscreen_mode = true
+
+-- 環境固有設定
+local ok, apply_local_config = pcall(require, "local")
+if ok and type(apply_local_config) == "function" then
+    apply_local_config(config, wezterm)
+end
 
 return config
