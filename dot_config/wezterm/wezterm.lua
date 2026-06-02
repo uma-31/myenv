d2aa17abcbbccd018d6a71ef93e54afef4fb2c1d
uMa <ysmst35@gmail.com>
2026-06-02 12:00:32 +0900

    WezTerm と Helix Editor のテーマを Tokyo Night Storm で合わせる

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,36 +2,8 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 -- 外観設定
-config.color_scheme = "GitHub Dark"
+config.color_scheme = "Tokyo Night Storm"
 config.font = wezterm.font("HackGen Console NF")
-
-config.background = {
-    {
-        source = {
-            Color = "black"
-        },
-        width = "100%",
-        height = "100%",
-        opacity = 0.6,
-    },
-}
-
-local background_image_candidates = wezterm.glob(
-    wezterm.config_dir .. "/background-image.*"
-)
-if #background_image_candidates > 0 then
-    table.insert(
-        config.background,
-        1,
-        {
-            source = {
-                File = background_image_candidates[1]
-            },
-            vertical_align = "Middle",
-            horizontal_align = "Center"
-        }
-    )
-end
 
 -- カーソル設定
 config.cursor_blink_rate = 800
